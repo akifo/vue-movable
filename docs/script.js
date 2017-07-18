27459ebe90cff4db5caddb269503d2169d0fcaaf
akifo <akiho@bb07.net>
2017-07-18 21:02:30 +0900

    Update documents

---
+++ docs/script.js
@@ -0,0 +1,13 @@
+// code highlight
+hljs.initHighlightingOnLoad()
+
+// Vue
+var app = document.querySelector('#app')
+new Vue({
+  el: app,
+  data: function () {
+    return { busy: false }
+  },
+  methods: {
+  }
+})
